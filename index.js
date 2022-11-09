67979616797b697c7f3533ab0be0a1cd8a9c763d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-09 20:12:42 +0530

    passed typeDefs and resolvers to apollo server

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const resolvers = {
     }
 }
 
-const server = new ApolloServer()
+const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({url}) => console.log("Server running on",url))
