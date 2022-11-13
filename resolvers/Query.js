ad71125264eb474f0fb85ad7c81d436e72544c53
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:25:58 +0530

    removed import & destructured dummydata from context

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,30 +1,34 @@
-import { categoriesData, productsData } from "../dummyDB.js"
 
 export const Query = {
     name: () => {
         return "Lakshit Chiranjiv Sagar"
     },
+
     age: () => {
         return 21
     },
+
     balance: () => {
         return 9999999.99999
     },
+
     isBest: () => true,
+
     roll: () => {
         return 586
     },
+    
     scores: () => [90,99,98],
-    products: () => productsData,
-    product: (parent,args,context) => {
-        const { id } = args
 
+    products: (parent,args,{productsData}) => productsData,
+    
+    product: (parent,{id},{productsData}) => {
         return productsData.find(p => p.id===id)
     },
-    categories: () => categoriesData,
-    category: (parent,args,context) => {
-        const { id } = args
-
+    
+    categories: (parent,args,{categoriesData}) => categoriesData,
+    
+    category: (parent,{id},{categoriesData}) => {
         return categoriesData.find(c => c.id === id)
     }
 }
