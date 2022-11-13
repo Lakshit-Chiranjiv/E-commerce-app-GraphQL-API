47b011260f7523ad2fb3f392b41f26ca35258e68
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:03:59 +0530

    imported dummy data

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
 import { ApolloServer } from "apollo-server";
+import { categoriesData, productsData } from "./dummyDB.js";
 import { typeDefs } from "./schema.js";
 
-const productsData = [
-    {
-        id: "pid1",
-        name: 'Laptop',
-        description: 'Good laptop',
-        price: 4885.23,
-        quantity: 30,
-        onSale: true,
-        categoryId: "cid1"
-    },
-    {
-        id: "pid2",
-        name: 'Smartphone',
-        description: 'Good smartphone',
-        price: 387.23,
-        quantity: 50,
-        onSale: true,
-        categoryId: "cid1"
-    },
-    {
-        id: "pid3",
-        name: 'Umbrella',
-        description: 'Good umbrella',
-        price: 45.73,
-        quantity: 10,
-        onSale: true,
-        categoryId: "cid3"
-    },
-    {
-        id: "pid4",
-        name: 'Notebook',
-        description: 'Good notebook',
-        price: 12.55,
-        quantity: 110,
-        onSale: false,
-        categoryId: "cid2"
-    }
-]
-
-const categoriesData = [
-    {
-        id: 'cid1',
-        name: 'Electronics'
-    },
-    {
-        id: 'cid2',
-        name: 'Stationary'
-    },
-    {
-        id: 'cid3',
-        name: 'Accessories'
-    }
-]
 
 const resolvers = {
     Query: {
