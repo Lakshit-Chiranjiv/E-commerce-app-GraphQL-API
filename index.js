bd211d0e869a1347b6e534f6e8ea4ac164bef695
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:13:37 +0530

    imported all resolvers

--- index.js
+++ index.js
@@ -1,52 +1,14 @@
 import { ApolloServer } from "apollo-server";
-import { categoriesData, productsData } from "./dummyDB.js";
+import { Category } from "./resolvers/Category.js";
+import { Product } from "./resolvers/Products.js";
+import { Query } from "./resolvers/Query.js";
 import { typeDefs } from "./schema.js";
 
 
 const resolvers = {
-    Query: {
-        name: () => {
-            return "Lakshit Chiranjiv Sagar"
-        },
-        age: () => {
-            return 21
-        },
-        balance: () => {
-            return 9999999.99999
-        },
-        isBest: () => true,
-        roll: () => {
-            return 586
-        },
-        scores: () => [90,99,98],
-        products: () => productsData,
-        product: (parent,args,context) => {
-            const { id } = args
-
-            return productsData.find(p => p.id===id)
-        },
-        categories: () => categoriesData,
-        category: (parent,args,context) => {
-            const { id } = args
-
-            return categoriesData.find(c => c.id === id)
-        }
-    },
-
-    Category: {
-        products: (parent,args,context) => {
-            const { id } = parent
-
-            return productsData.filter(p => p.categoryId===id)
-        }
-    },
-
-    Product: {
-        category: (parent,args,context) => {
-            const { categoryId } = parent
-            return categoriesData.find(c => c.id===categoryId)
-        }
-    }
+    Query,
+    Category,
+    Product
 }
 
 const server = new ApolloServer({typeDefs, resolvers})
