0678f223b6785c1a78ddde314f426c70e152e80c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:43:34 +0530

    added reviews dummy data

--- dummyDB.js
+++ dummyDB.js
@@ -51,3 +51,62 @@ export const categoriesData = [
         name: 'Accessories'
     }
 ]
+
+export const reviewsData = [
+    {
+        id: "rid1",
+        title: "Great product",
+        comment: "This product has really been very much helpful",
+        rating: 4,
+        date: "07-11-2022",
+        productId: "pid2"
+    },
+    {
+        id: "rid2",
+        title: "Not so cool",
+        comment: "Was quite good, but over the time it really pissed me off",
+        rating: 2,
+        date: "18-09-2022",
+        productId: "pid3"
+    },
+    {
+        id: "rid3",
+        title: "Up to the mark",
+        comment: "Won't say very good, but it was up to the expectations",
+        rating: 3,
+        date: "02-05-2021",
+        productId: "pid4"
+    },
+    {
+        id: "rid4",
+        title: "Awesome",
+        comment: "Its really awesome and wonderful",
+        rating: 5,
+        date: "26-04-2021",
+        productId: "pid1"
+    },
+    {
+        id: "rid5",
+        title: "Total waste",
+        comment: "It was really a waste of time and money, really disappointed",
+        rating: 1,
+        date: "12-10-2020",
+        productId: "pid2"
+    },
+    {
+        id: "rid6",
+        title: "Nice one",
+        comment: "Bit of queries but still it was useful and good",
+        rating: 4,
+        date: "17-09-2021",
+        productId: "pid1"
+    },
+    {
+        id: "rid7",
+        title: "Too bad",
+        comment: "It was already a broken one, so bad service",
+        rating: 1,
+        date: "11-07-2022",
+        productId: "pid3"
+    },
+]
