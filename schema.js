6460a6eb73992ad912876171117ad6c44d02059c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:01:00 +0530

    schema file to store typeDefs

---
+++ schema.js
@@ -0,0 +1,33 @@
+import { gql } from 'apollo-server'
+
+export const typeDefs = gql`
+    type Query {
+        name: String,
+        age: Int,
+        balance: Float,
+        isBest: Boolean,
+        roll: Int!,
+        scores: [Int!]!,
+        products: [Product!]!,
+        product(id: ID!): Product,
+        categories: [Category!]!,
+        category(id: ID!): Category
+        # ! specifies it can't be null
+    }
+
+    type Product {
+        id: ID!,
+        name: String!,
+        description: String,
+        price: Float!,
+        quantity: Int!,
+        onSale: Boolean!,
+        category: Category
+    }
+
+    type Category {
+        id: ID!,
+        name: String!,
+        products: [Product!]
+    }
+`
