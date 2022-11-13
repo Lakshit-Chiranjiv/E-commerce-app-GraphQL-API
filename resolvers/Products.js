0b0f4387ec5f75f219fdfdd517260c88505b35bb
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:48:50 +0530

    added reviews resolver in Product

--- resolvers/Products.js
+++ resolvers/Products.js
@@ -4,5 +4,11 @@ export const Product = {
         const { categoriesData } = context
         const { categoryId } = parent
         return categoriesData.find(c => c.id===categoryId)
+    },
+
+    reviews: (parent,args,context) => {
+        const { reviewsData } = context
+        const { id } = parent
+        return reviewsData.filter(r => r.productId===id)
     }
 }
