c40c56c4c5db86708d4ca5d447fb234d4254e527
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:03:15 +0530

    separated dummy data and exported

---
+++ dummyDB.js
@@ -0,0 +1,53 @@
+export const productsData = [
+    {
+        id: "pid1",
+        name: 'Laptop',
+        description: 'Good laptop',
+        price: 4885.23,
+        quantity: 30,
+        onSale: true,
+        categoryId: "cid1"
+    },
+    {
+        id: "pid2",
+        name: 'Smartphone',
+        description: 'Good smartphone',
+        price: 387.23,
+        quantity: 50,
+        onSale: true,
+        categoryId: "cid1"
+    },
+    {
+        id: "pid3",
+        name: 'Umbrella',
+        description: 'Good umbrella',
+        price: 45.73,
+        quantity: 10,
+        onSale: true,
+        categoryId: "cid3"
+    },
+    {
+        id: "pid4",
+        name: 'Notebook',
+        description: 'Good notebook',
+        price: 12.55,
+        quantity: 110,
+        onSale: false,
+        categoryId: "cid2"
+    }
+]
+
+export const categoriesData = [
+    {
+        id: 'cid1',
+        name: 'Electronics'
+    },
+    {
+        id: 'cid2',
+        name: 'Stationary'
+    },
+    {
+        id: 'cid3',
+        name: 'Accessories'
+    }
+]
