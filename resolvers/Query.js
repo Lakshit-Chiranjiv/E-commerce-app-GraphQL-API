301f1eeda630c7b299e0ad1f99d447dd57115b72
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-13 12:08:13 +0530

    separate query resolvers

---
+++ resolvers/Query.js
@@ -0,0 +1,28 @@
+export const Query = {
+    name: () => {
+        return "Lakshit Chiranjiv Sagar"
+    },
+    age: () => {
+        return 21
+    },
+    balance: () => {
+        return 9999999.99999
+    },
+    isBest: () => true,
+    roll: () => {
+        return 586
+    },
+    scores: () => [90,99,98],
+    products: () => productsData,
+    product: (parent,args,context) => {
+        const { id } = args
+
+        return productsData.find(p => p.id===id)
+    },
+    categories: () => categoriesData,
+    category: (parent,args,context) => {
+        const { id } = args
+
+        return categoriesData.find(c => c.id === id)
+    }
+}
