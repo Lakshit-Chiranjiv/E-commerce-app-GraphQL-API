92bceb71cca2b7fd8a325cda4702ed5539e10597
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-15 00:12:28 +0530

    avgRating filter resolver in Query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -20,13 +20,30 @@ export const Query = {
     
     scores: () => [90,99,98],
 
-    products: (parent,{filter},{productsData}) => {
+    products: (parent,{filter},{productsData, reviewsData}) => {
         let filteredProducts = productsData
         if(filter){
-            if(filter.onSale)
+            const { onSale, avgRating } = filter
+            if(onSale)
                 filteredProducts = productsData.filter(p => p.onSale)
             else
                 filteredProducts = productsData.filter(p => !p.onSale)  
+
+            if([1,2,3,4,5].includes(avgRating)){
+                filteredProducts = filteredProducts.filter(p => {
+                    let sumRating = 0, reviewCount = 0
+                
+                    reviewsData.forEach(r => {
+                        if(r.productId === p.id){
+                            sumRating += r.rating
+                            reviewCount++
+                        }
+                    })
+                    const avgProductRating = Math.floor(sumRating/reviewCount)
+
+                    return avgProductRating===avgRating
+                })
+            }
         }
         return filteredProducts
     },
