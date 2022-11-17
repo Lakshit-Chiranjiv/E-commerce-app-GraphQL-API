e11912a759c54fde6d03218af2ed209532d28e03
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-18 00:17:04 +0530

    imported and added mutation resolvers

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { ApolloServer } from "apollo-server";
 import { categoriesData, productsData, reviewsData } from "./dummyDB.js";
 import { Category } from "./resolvers/Category.js";
+import { Mutation } from "./resolvers/Mutation.js";
 import { Product } from "./resolvers/Products.js";
 import { Query } from "./resolvers/Query.js";
 import { typeDefs } from "./schema.js";
 
 const resolvers = {
     Query,
+    Mutation,
     Category,
     Product
 }
